824e953919002fcdeee74b3a0c72f3c3daff5b8b
CygnusX-26 <42686923+CygnusX-26@users.noreply.github.com>
2022-11-23 21:19:16 -0500

    test

--- FlaskApp/index.py
+++ FlaskApp/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from bot.sql_methods import getLink, getAll
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route('/<name>')
 def index(name):
     return redirect(getLink(name)[0])
+
+@app.route('/')
+def all():
+    return render_template('index.html')
 
 
 app.run()
