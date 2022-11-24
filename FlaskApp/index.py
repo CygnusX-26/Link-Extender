0485111f202e10298faca98026ea667b8a255efb
CygnusX-26 <42686923+CygnusX-26@users.noreply.github.com>
2022-11-23 22:06:03 -0500

    stuff

--- FlaskApp/index.py
+++ FlaskApp/index.py
@@ -1,16 +1,40 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from emoji import random_emoji
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from bot.sql_methods import getLink, getAll
+from bot.sql_methods import getLink, getAll, insertLink
+import validators
+import lorem
+
 app = Flask(__name__)
 
 @app.route('/<name>')
-def index(name):
+def url(name):
     return redirect(getLink(name)[0])
 
-@app.route('/')
-def all():
-    return render_template('index.html')
+@app.route('/', methods=['GET'])
+def index():
+    if (request.method == 'GET' and request.args.get('inputurl') and request.args.get('fillertype')):
+        url = request.args.get('inputurl')
+        filler = request.args.get('fillertype')
+        if (filler == 'latin'):
+            if not validators.url(url):
+                return render_template('index.html', show=True)
+            extended = ""
+            for _ in range(5):
+                extended += lorem.paragraph()
+            extended = extended.replace(' ', '-')
+            extended = extended.replace('.', '')
+            extended = extended.replace(',', '')
+            insertLink(url, extended)
+            return render_template('index.html', show=False, url=url, extended=extended)
+        elif (filler == 'copypasta'):
+            extended = ""
+            while True:
+                extended += random_emoji()[0]
+                break
+            pass
+    return render_template('index.html', show=True)
 
 
 app.run()
